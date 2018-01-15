0f7da0b5f6cc8b97a8f3977c4e5f7efa8f85b295
Sergey Maslov <maslovsa@gmail.com>
2018-01-15 17:30:30 +0400

    add Carthage support

---
+++ WebimClientLibrary/WebimClientLibrary.h
@@ -0,0 +1,19 @@
+//
+//  WebimClientLibrary.h
+//  WebimClientLibrary
+//
+//  Created by Sergey Maslov on 15.01.2018.
+//  Copyright © 2018 Sergey Maslov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WebimClientLibrary.
+FOUNDATION_EXPORT double WebimClientLibraryVersionNumber;
+
+//! Project version string for WebimClientLibrary.
+FOUNDATION_EXPORT const unsigned char WebimClientLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WebimClientLibrary/PublicHeader.h>
+
+
